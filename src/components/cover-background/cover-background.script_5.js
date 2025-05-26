a8e01234a60dcf50d2e2974b3ade495d080804ec
Christopher serna flores <christophersernaflores@gmail.com>
2025-05-26 09:58:41 -0600

    UPDATE COVER GEOMETRY

--- src/components/cover-background/cover-background.script_5.js
+++ src/components/cover-background/cover-background.script_5.js
@@ -1,3 +1,4 @@
+const breakpoint = 500
 const images_langs_frameworks_names = [
     'angular',
     'dart',
@@ -18,20 +19,23 @@ const images_langs_frameworks_names = [
     'ts',
     'vue'
 ];
-const showedImages = [];
-
-var load_images = [];
+const showed_images = [];
+var loaded_images = [];
+var past_image_index = null
+var past_x = 0
 
 export const init = (canvas) => {
+    past_x = Math.random() * canvas.width
     for (let i = 0; i < 5; i++) {
-        showedImages.push(createImage(canvas));
+        showed_images.push(createImage(canvas));
     }
 }
 
 export const preloadAllImages = async () => {
     try {
         const images = await Promise.all(images_langs_frameworks_names.map(name => loadImage(`/lang-frameworks-background/${name}.png`)));
-        load_images = images
+        loaded_images = images
+        past_image_index = Math.floor(Math.random() * (loaded_images.length - 1))
     } catch (err) {
         console.error("One or more images failed to load:", err)
     }
@@ -44,48 +48,74 @@ export const loadImage = (src) => {
         img.onload = () => resolve(img)
         img.onerror = () => reject(new Error(`Failed to load: ${src}`))
         img.src = src
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.objectFit = 'contain';
     });
 }
 
 export const createImage = (canvas) => {
 
-    const image_index = Math.floor(Math.random() * load_images.length)
-    const image = load_images[image_index]
+    const separation = canvas.width > breakpoint ? 200 : 50
+    const limit = canvas.width > breakpoint ? canvas.width : canvas.width * 2
 
-    const min_width = canvas.width > 500 ? canvas.width / 5 : canvas.width
+    past_image_index++
+    if(past_image_index >= loaded_images.length) past_image_index = 0
+
+    past_x += separation
+    if(past_x >= limit) past_x = 0
+
+    const min_width = canvas.width > breakpoint ? canvas.width / 5 : canvas.width / 2
+    const grow = canvas.width > breakpoint ? 0.3 : 0.2
+    const image_index = past_image_index
+    const x = min_width + past_x
+    const y = canvas.height + Math.random() * 100
+    const image = loaded_images[image_index]
+    const speed = 0.001 + Math.random() * 0.003
 
     return {
-        x: min_width + Math.random() * canvas.width,
-        y: (canvas.height - 200) + Math.random() * 500,
-        image: image,
+        id: image_index,
+        x,
+        y,
+        grow,
+        image,
         opacity: 1,
         size: 0,
-        speed: 0.001 + Math.random() * 0.001,
-        direction: Math.random() * 1
+        speed
     };
 }
 
 export const draw = (canvas, ctx) => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let p of showedImages) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.globalAlpha = p.opacity
-    ctx.drawImage(p.image, p.x, p.y, p.size, p.size)
-    ctx.globalAlpha = 1.0
+    for (let p of showed_images) {
 
-    if (p.opacity > 0) {
-        p.opacity -= p.speed
-        p.size += 0.35
-        p.x -= 2
-        p.y -= 2
+        try {
+        
+            ctx.globalAlpha = p.opacity
+            ctx.drawImage(p.image, p.x, p.y, p.size, p.size)
+            ctx.globalAlpha = 1.0
+        
+            if (p.opacity > 0) {
+                p.opacity -= p.speed
+                p.size += p.grow
+                p.x -= 1
+                p.y -= 1
+            }
+            if (p.opacity <= 0) {
+                Object.assign(p, createImage(canvas));
+            }
+
+        }
+        catch(err) {
+            console.error('The image can be draw: ', err)
+            console.log(p)
+        }
+
     }
-    if (p.opacity <= 0) {
-        Object.assign(p, createImage(canvas));
-    }
-  }
 
-  requestAnimationFrame(() => draw(canvas, ctx));
+    requestAnimationFrame(() => draw(canvas, ctx));
 }
 
 export const addResizeListener = (canvas) => {
